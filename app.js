9612fdcead91d28bbfb18cd8c86e47bb00ce8875
Asif Shahzad <aqrab89@gmail.com>
2020-09-03 00:15:19 +0500

    signup with error handling

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 
 const userRoutes = require('./routes/user');
@@ -12,6 +15,13 @@ mongoose.connect(process.env.DATABASE, {
   useNewUrlParser: true,
   useCreateIndex: true
 }).then(() => console.log("DB Connected"));
+
+
+// middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
 
 // routes middleware
 app.use("/api", userRoutes);
