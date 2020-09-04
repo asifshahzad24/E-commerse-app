16b4105b438f25e54d5a48d04a862141d1c0b8e6
Asif Shahzad <aqrab89@gmail.com>
2020-09-05 00:53:52 +0500

    signin with jwt and authenticate

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const jwt = require("jsonwebtoken");
+const expresJwt = require("express-jwt");
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
 exports.signup = (req, res) => {
@@ -17,4 +19,42 @@ exports.signup = (req, res) => {
             user
         });
     });
+};
+
+exports.signin = (req, res) => {
+
+    // find user based on email
+    const { email, password } = req.body;
+    User.findOne({ email }, (err, user) => {
+        if (err || !user) {
+            return res.status(400).json({
+                error: "User with this email does not exist, Pleas signup"
+            });
+        }
+        // if user found make sure the email and password match
+        // create authenticate method in user model
+        if (!user.authenticate(password)) {
+            return res.status(401).json({
+                error: "Email and Password don't matched"
+            });
+        }
+
+        // generate a signed token with userid and secret 
+        const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET);
+
+        // persit the token as "lt" in cookie with expiry date
+        res.cookie('tl', token, { expire: new Date() + 9999 });
+
+        // return reponse with user and token to fronend client 
+
+        const { _id, name, email, role } = user;
+        return res.json({ token, user: { _id, name, email, role } });
+
+    });
+};
+
+exports.signout = (req, res) => {
+
+    res.clearCookie('tl');
+    res.json({ message: "Signout Success" })
 };
--- models/user.js
+++ models/user.js
@@ -30,32 +30,36 @@ const userSchema = new mongoose.Schema({
         type: Array,
 
     }
- },
- { timestamps: true }
+},
+    { timestamps: true }
 );
 
 //virtual field
 userSchema.virtual('password')
-.set(function(password) {
+    .set(function (password) {
 
-    this._password = password,
-    this.salt = uuidv1(),
-    this.hashed_password = this.encryptPassword(password)
+        this._password = password,
+            this.salt = uuidv1(),
+            this.hashed_password = this.encryptPassword(password)
 
-})
-.get(function() {
-    return this._password
-})
+    })
+    .get(function () {
+        return this._password
+    })
 
 //encrypt password
 userSchema.methods = {
-    encryptPassword: function(password) {
-        if(!password) return '';
+    authenticate: function (plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password;
+    },
+
+    encryptPassword: function (password) {
+        if (!password) return '';
         try {
             return crypto.createHmac('sha1', this.salt)
-                   .update(password)
-                   .digest('hex');
-        } catch (err){
+                .update(password)
+                .digest('hex');
+        } catch (err) {
 
             return "";
         }
