5a48b4d365addfa94a52e3db05495a3e753a3d62
Asif Shahzad <aqrab89@gmail.com>
2020-09-08 01:20:28 +0500

    all apis for product and category

--- routes/category.js
+++ routes/category.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require("../controllers/category");
+const { create, categoryById, read, update, remove, list } = require("../controllers/category");
 const { requireSignin, isAuth, isAdmin } = require("../controllers/auth");
 const { userById } = require("../controllers/user");
 
@@ -12,6 +12,23 @@ router.post('/category/create/:userId',
     create
 );
 
+router.get('/category/:categoryId', read);
+router.get('/categories', list);
+
+router.delete('/category/:categoryId/:userId',
+    requireSignin,
+    isAuth,
+    isAdmin,
+    remove,
+);
+router.put('/category/:categoryId/:userId',
+    requireSignin,
+    isAuth,
+    isAdmin,
+    update,
+);
+
+router.param('categoryId', categoryById);
 router.param('userId', userById);
 
 module.exports = router;
