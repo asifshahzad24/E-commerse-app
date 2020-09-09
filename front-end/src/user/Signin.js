34838c739e40621d2d8dddc2a40b57d1d31bc6ca
Asif Shahzad <aqrab89@gmail.com>
2020-09-09 23:40:34 +0500

    Setup front end app

---
+++ front-end/src/user/Signin.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../core/Layout";
+
+const Signin = () => (
+    <Layout title="Signin" description="Signin Node React E-Commerce App">
+        ...
+    </Layout>
+);
+
+export default Signin;
---
+++ front-end/src/user/Signup.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Layout from "../core/Layout";
+import { API } from "../config";
+
+const Signup = () => (
+    <Layout title="Signup" description="Signup Node React E-Commerce App">
+        {API}
+    </Layout>
+);
+
+export default Signup;
