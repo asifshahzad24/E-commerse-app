1d6a2c772eee45f3f0d2cbab908ec839a0428a42
Asif Shahzad <aqrab89@gmail.com>
2020-09-22 20:08:14 +0500

    cart

--- front-end/src/core/Home.js
+++ front-end/src/core/Home.js
@@ -44,7 +44,7 @@ const Home = () => {
       <h2 className="mb-4">New Arrival</h2>
       <div className="row">
         {productsByArrival.map((product, i) => (
-          <div className="col-4 mb-3">
+          <div key={i} className="col-4 mb-3">
             <Card key={i} product={product} />
           </div>
         ))}
@@ -53,7 +53,7 @@ const Home = () => {
       <h2 className="mb-4">Best Sellers</h2>
       <div className="row">
         {productsBySell.map((product, i) => (
-          <div className="col-4 mb-3">
+          <div key={i} className="col-4 mb-3">
             <Card key={i} product={product} />
           </div>
         ))}
