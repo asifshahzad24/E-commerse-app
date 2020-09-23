1d0b63f3a782d1079b304308ca04fe51eab0ff08
Asif Shahzad <aqrab89@gmail.com>
2020-09-23 15:47:23 +0500

    Add files via upload

--- api/controllers/braintree.js
+++ api/controllers/braintree.js
@@ -3,11 +3,11 @@ const braintree = require("braintree");
 require("dotenv").config();
 
 var gateway = new braintree.BraintreeGateway({
-    environment: braintree.Environment.Sandbox,
-    merchantId: process.env.BRAINTREE_MERCHANT_ID,
-    publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-    privateKey: process.env.BRAINTREE_PRIVATE_KEY,
-  }); 
+  environment: braintree.Environment.Sandbox,
+  merchantId: process.env.BRAINTREE_MERCHANT_ID,
+  publicKey: process.env.BRAINTREE_PUBLIC_KEY,
+  privateKey: process.env.BRAINTREE_PRIVATE_KEY,
+});
 
 exports.generateToken = (req, res) => {
   gateway.clientToken.generate({}, function (err, response) {
@@ -17,4 +17,27 @@ exports.generateToken = (req, res) => {
       res.send(response);
     }
   });
+};
+
+exports.processPayment = (req, res) => {
+  let nonceFromClient = req.body.paymentMethodNonce;
+  let amountClient = req.body.amount;
+
+  //charge
+  let newTransaction = gateway.transaction.sale(
+    {
+      amount: amountClient,
+      paymentMethodNonce: nonceFromClient,
+      options: {
+        submitForSettlement: true,
+      },
+    },
+    (error, result) => {
+      if (error) {
+        res.status(500).json(error);
+      } else {
+        res.json(result);
+      }
+    }
+  );
 };
