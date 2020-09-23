0e3f162b24dd1e78483070a4dc85c3c308183fbd
Asif Shahzad <aqrab89@gmail.com>
2020-09-23 19:56:54 +0500

    order

---
+++ api/routes/order.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const { requireSignin, isAuth } = require("../controllers/auth");
+const { userById, addOrderToUser } = require("../controllers/user");
+const { create } = require("../controllers/order");
+const { decreaseQuantity } = require("../controllers/product");
+
+router.post(
+  "/order/create/:userId",
+  requireSignin,
+  isAuth,
+  addOrderToUser,
+  decreaseQuantity,
+  create
+);
+
+router.param("userId", userById);
+module.exports = router;
