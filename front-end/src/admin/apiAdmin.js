7e388e8a08ac1b4970b11339cdc2a9196dc373f0
Asif Shahzad <aqrab89@gmail.com>
2020-09-27 02:20:33 +0500

    order manage

--- front-end/src/admin/apiAdmin.js
+++ front-end/src/admin/apiAdmin.js
@@ -13,7 +13,7 @@ export const createCategory = (userId, token, category) => {
         .then(response => {
             return response.json();
         })
-        .catch(err => { 
+        .catch(err => {
             console.log(err);
         });
 };
@@ -30,7 +30,7 @@ export const createProduct = (userId, token, product) => {
         .then(response => {
             return response.json();
         })
-        .catch(err => { 
+        .catch(err => {
             console.log(err);
         });
 };
@@ -38,12 +38,64 @@ export const createProduct = (userId, token, product) => {
 export const getCategories = () => {
     return fetch(`${API}/categories`, {
         method: "GET",
-        
+
     })
         .then(response => {
             return response.json();
         })
-        .catch(err => { 
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+export const listOrders = (userId, token) => {
+    return fetch(`${API}/order/list/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+export const getStatusValues = (userId, token) => {
+    return fetch(`${API}/order/status-value/${userId}`, {
+        method: "GET",
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+export const updateOrderStatus = (userId, token, orderId, status) => {
+    return fetch(`${API}/order/${orderId}/status/${userId}`, {
+        method: "PUT",
+        headers: {
+            Accept: 'application/json',
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify({ status, orderId }),
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
             console.log(err);
         });
 };
